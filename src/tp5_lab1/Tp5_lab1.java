0c4ebbb4c7f5617b8c997dab0079f7eece015244
Thiagojo08 <thiagotavarez015@gmail.com>
2023-08-24 18:45:37 -0300

    Initial commit

---
+++ src/tp5_lab1/Tp5_lab1.java
@@ -0,0 +1,17 @@
+
+package tp5_lab1;
+
+
+public class Tp5_lab1 {
+
+    
+    public static void main(String[] args) {
+     
+        
+        
+        System.out.println("Hola mundo");
+        
+        
+    }
+    
+}
